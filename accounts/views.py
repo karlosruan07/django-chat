a420f5988924dfddbf67aab5aacff6b8a69e3a32
ArthurSchumacher <90356436+ArthurSchumacher@users.noreply.github.com>
2021-09-08 23:44:16 -0300

    Views / Urls / Templates
    Adicionando uma parte do front-end.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def Home(request):
+    return render(request, 'accounts/home-content.html')
+
+def Login(request):
+    return render(request, 'accounts/login-content.html')
+
+def Signup(request):
+    return render(request, 'accounts/signup-content.html')
